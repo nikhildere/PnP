b0c8936b6dd39e799ba544507f2aff5b219e9a1a
nikhildere <nikhil.dere@live.com>
2015-07-20 15:49:14 +0530

    Blog webpart chrome

--- Solutions/Provisioning.UX.App/Provisioning.Job/MdlzComponents/ExtensibilityProviders/MdlzBlogSiteExtensibilityProvider.cs
+++ Solutions/Provisioning.UX.App/Provisioning.Job/MdlzComponents/ExtensibilityProviders/MdlzBlogSiteExtensibilityProvider.cs
@@ -6,8 +6,10 @@ using Provisioning.Job.MdlzComponents.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls.WebParts;
 
 namespace Provisioning.Job.MdlzComponents.ExtensibilityProviders
 {
@@ -15,7 +17,29 @@ namespace Provisioning.Job.MdlzComponents.ExtensibilityProviders
     {
         public void ProcessRequest(ClientContext ctx, ProvisioningTemplate template, string configurationData)
         {
-            
+            Web web = ctx.Web;
+            var props = new List<Expression<Func<Web, object>>>();
+            if (!web.IsPropertyAvailable("ServerRelativeUrl"))
+                props.Add(x => x.ServerRelativeUrl);
+            if (web.RootFolder.ServerObjectIsNull != false)
+                props.Add(x => x.RootFolder);
+
+            if (props.Count > 0)
+            {
+                ctx.Load(web, props.ToArray());
+                ctx.ExecuteQuery();
+            }
+
+            //var homepageFile = web.GetFileByServerRelativeUrl(UrlUtility.Combine(web.ServerRelativeUrl, web.RootFolder.WelcomePage));
+
+            var webparts = web.GetWebParts(UrlUtility.Combine(web.ServerRelativeUrl, web.RootFolder.WelcomePage));
+
+            foreach (var item in webparts)
+            {
+                item.WebPart.Properties["ChromeType"] = PartChromeType.Default;
+                item.SaveWebPartChanges();
+            }
+            ctx.ExecuteQuery();
         }
     }
 
